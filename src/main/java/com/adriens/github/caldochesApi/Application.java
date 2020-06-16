acfc306aa20319f7ce41ddf27690ddc131d435f9
Meilie <bossart.emilie@gmail.com>
2020-06-17 10:19:45 +1100

    spring boot first load
    w/ jpa, web, h2db, liquibase, test and validation dependencies in pom.xml
    h2 console, datasource, jpa and liquibase configurations in application.properties

---
+++ src/main/java/com/adriens/github/caldochesApi/Application.java
@@ -0,0 +1,13 @@
+package com.adriens.github.caldochesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriens/github/caldochesApi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriens.github.caldochesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
