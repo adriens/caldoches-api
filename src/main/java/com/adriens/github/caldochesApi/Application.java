0c023eb5786629add7f8b8e293d6c2fe696258bf
Meilie <bossart.emilie@gmail.com>
2020-06-17 15:17:47 +1100

    add javadoc

--- src/main/java/com/adriens/github/caldochesApi/Application.java
+++ src/main/java/com/adriens/github/caldochesApi/Application.java
@@ -3,10 +3,18 @@ package com.adriens.github.caldochesApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author meilie
+ */
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    /**
+     * main application
+     * @param args
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
