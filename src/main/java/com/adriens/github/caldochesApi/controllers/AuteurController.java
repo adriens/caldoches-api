ce6678e1538e8cda26db72800ab0cef77f68e60a
Meilie <bossart.emilie@gmail.com>
2020-06-17 15:20:11 +1100

    remove useless imports, remove createAuteur, remove RequestMapping
    #2

--- src/main/java/com/adriens/github/caldochesApi/controllers/AuteurController.java
+++ src/main/java/com/adriens/github/caldochesApi/controllers/AuteurController.java
@@ -11,16 +11,11 @@ import com.adriens.github.caldochesApi.services.AuteurService;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author meilie
  */
 @RestController
-@RequestMapping("/auteurs")
 public class AuteurController {
     
     @Autowired
@@ -52,15 +46,6 @@ public class AuteurController {
     @GetMapping("/auteurs/{id}")
     public ResponseEntity<Auteur> getAuteurById(@PathVariable(value = "id") Long auteurId) throws ResourceNotFoundException {
         return ResponseEntity.ok().body(auteurService.getAuteur(auteurId));
-    }
-
-    /**
-     *
-     * @param auteur
-     */
-    @PostMapping("/create")
-    public void createAuteur(@Valid @RequestBody Auteur auteur) {
-        auteurService.saveAuteur(auteur);
     }
 
 }
