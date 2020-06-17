0217097bf6a38ccd691e44db6610e609a28bd64a
Meilie <bossart.emilie@gmail.com>
2020-06-17 15:24:01 +1100

    add field cleAuteur, add javadoc
    #1

--- src/main/java/com/adriens/github/caldochesApi/entities/Auteur.java
+++ src/main/java/com/adriens/github/caldochesApi/entities/Auteur.java
@@ -27,45 +27,83 @@ public class Auteur {
     @Column(nullable = false)
     private String nom;
     
+    @Column(nullable = false)
+    private String cleAuteur;
+    
+    /**
+     * constructor
+     */
     protected Auteur() {
         // for JPA
     }
     
-    public Auteur(long id, String nom) {
+    /**
+     * constructor
+     * @param id
+     * @param nom
+     * @param cleAuteur
+     */
+    public Auteur(long id, String nom, String cleAuteur) {
         this.id = id;
         this.nom = nom;
+        this.cleAuteur = cleAuteur;
     }
 
     /**
-     * @return the id
+     * get the author id
+     * @return the id of the author
      */
     public long getId() {
         return id;
     }
 
     /**
-     * @param id the id to set
+     * set the author id
+     * @param id the author's id to set
      */
     public void setId(long id) {
         this.id = id;
     }
 
     /**
-     * @return the nom
+     * get the author name
+     * @return the name of the author
      */
     public String getNom() {
         return nom;
     }
 
     /**
-     * @param nom the nom to set
+     * set the author name
+     * @param nom the author's name to set
      */
     public void setNom(String nom) {
         this.nom = nom;
     }
     
+    /**
+     * get the author keyword
+     * @return the keyword of the author
+     */
+    public String getCleAuteur() {
+        return cleAuteur;
+    }
+
+    /**
+     * set the author keyword
+     * @param cleAuteur the author's keyword to set
+     */
+    public void setCleAuteur(String cleAuteur) {
+        this.cleAuteur = cleAuteur;
+    }
+    
+    /**
+     * get the author to String
+     * @return the author as text
+     */
     @Override
     public String toString() {
         return "Auteur{id: "+id+", nom: "+nom+"}";
     }
+
 }
