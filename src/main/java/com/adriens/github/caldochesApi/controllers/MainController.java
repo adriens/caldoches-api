2cc360bdfc89ea9a56048d2fd27790787569986f
Meilie <bossart.emilie@gmail.com>
2020-07-10 08:26:51 +1100

    redirect to doc path
    redirect "/" to "doc.kalolo.html" 
    #7

---
+++ src/main/java/com/adriens/github/caldochesApi/controllers/MainController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adriens.github.caldochesApi.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ *
+ * @author meilie
+ */
+@RestController
+public class MainController {
+    
+    @GetMapping("/")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public RedirectView getDefault() throws Exception {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/doc.kalolo.html");
+        return redirectView;
+    }
+    
+}
